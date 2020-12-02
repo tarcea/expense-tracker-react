938c8e24cdf63707c4ffec09e7bdfb4e9924e978
Gheorghe Tarcea <tarcea@gmail.com>
2020-12-02 11:30:00 +0100

    all functionalities in place, no db

---
+++ src/components/Transaction.js
@@ -0,0 +1,19 @@
+import React, { useContext } from 'react';
+import { GlobalContext } from '../context/GlobalState';
+
+const Transaction = ({ transaction }) => {
+  const sign = transaction.amount < 0 ? '-' : '+';
+  const { deleteTransaction } = useContext(GlobalContext);
+
+  return (
+    <div>
+      <li className={sign === '-' ? "minus" : "plus"}>
+        {transaction.text}
+        <span>{sign}${Math.abs(transaction.amount)}</span>
+        <button className="delete-btn" onClick={() => deleteTransaction(transaction.id)}>x</button>
+      </li>
+    </div>
+  )
+}
+
+export default Transaction;
